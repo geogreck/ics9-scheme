f5f5b3b475b07a23ae3aeb0e5cc46b4ac5acc12a
Starovoytov Alexandr <saniy200311@gmail.com>
2021-12-01 14:34:38 +0300

    refactor definitions
    Now =define= command stores definition as (name index-of-first-command)

--- labs/lab5.scm
+++ labs/lab5.scm
@@ -6,15 +6,13 @@
 
 ;; -> stack после выполнения program
 (define (interpret program stack)
-  (let interpret-internal ((program program)
-                           (stack stack)
+  (let interpret-internal ((stack stack)
                            (index 0)
                            (dictionary '()))
     (if (= index (vector-length program))
         stack
         (let ((command (vector-ref program index))
-              (arithmetic? (lambda (command)
-                             (member command '(+ - * / mod neg nigger))))
+              (arithmetic? (lambda (command)  (member command '(+ - * / mod neg nigger))))
               (arithmetic (lambda (command stack)
                             (cond
                              ((equal? command '+) (cons (+ (cadr stack) (car stack)) (cddr stack)))
@@ -64,14 +62,6 @@
                              ((equal? 'over command) (cons (cadr stack) stack))
                              ((equal? 'rot command) (cons (caddr stack) (cons (cadr stack) (cons (car stack) (cdddr stack)))))
                              ((equal? 'depth command)(cons (length stack) stack)))))
-              (definition (lambda () ;; -> '(word #())
-                            (list (vector-ref program (+ index 1))
-                                  (let loop ((index (+ index 2))
-                                             (res '()))
-                                    (let ((current (vector-ref program index)))
-                                      (if (equal? 'end current)
-                                          (list->vector (reverse res))
-                                          (loop (+ index 1) (cons current res))))))))
               (iffish (lambda () ;; -> #()
                         (let loop ((index (+ index 1))
                                    (res '()))
@@ -80,60 +70,53 @@
                                 (list->vector (reverse res))
                                 (loop (+ index 1) (cons current res))))))))
           (cond ((number? command)
-                 (interpret-internal program
-                                     (cons command stack)
+                 (interpret-internal (cons command stack)
                                      (+ index 1)
                                      dictionary))
                 ((arithmetic? command)
-                 (interpret-internal program
-                                     (arithmetic command stack)
+                 (interpret-internal (arithmetic command stack)
                                      (+ index 1)
                                      dictionary))
                 ((compare? command)
-                 (interpret-internal program
-                                     (compare command stack)
+                 (interpret-internal (compare command stack)
                                      (+ index 1)
                                      dictionary))
                 ((logic? command)
-                 (interpret-internal program
-                                     (logic command stack)
+                 (interpret-internal (logic command stack)
                                      (+ index 1)
                                      dictionary))
                 ((stack-anya? command)
-                 (interpret-internal program
-                                     (stack-anya command stack)
+                 (interpret-internal (stack-anya command stack)
                                      (+ index 1)
                                      dictionary))
                 ((equal? 'define command)
-                 (let* ((new-proc (definition))
-                        (val (vector-length (cadr new-proc))))
-                   (interpret-internal program
-                                       stack
-                                       (+ index val 3)
-                                       (cons new-proc dictionary))))
+                 (interpret-internal stack
+                                     (+ 1 ;; следующее слово за end
+                                        (let loop ((index (+ index 2))) ;; вернет индекс следующего end
+                                          (if (equal? 'end (vector-ref program index))
+                                              index
+                                              (loop (+ index 1)))))
+                                     (cons (list (vector-ref program (+ index 1)) (+ index 2)) ;; (название-статьи индекс-первого-слова-статьи)
+                                           dictionary)))
                 ((assoc command dictionary) => (lambda (called-proc)
-                                                 (interpret-internal program
-                                                                     (interpret-internal (cadr called-proc)
-                                                                                         stack
-                                                                                         0
+                                                 (interpret-internal (interpret-internal stack
+                                                                                         (cadr called-proc)
                                                                                          dictionary)
                                                                      (+ index 1)
                                                                      dictionary)))
                 ((equal? 'exit command) stack)
+                ((equal? 'end command) stack)
                 ((equal? 'if command)
                  (let* ((body (iffish))
                         (val (vector-length body)))
                    (if (= 0 (car stack))
-                       (interpret-internal program
-                                           (cdr stack)
+                       (interpret-internal (cdr stack)
                                            (+ index val 2)
                                            dictionary)
-                       (interpret-internal program
-                                           (cdr stack)
+                       (interpret-internal (cdr stack)
                                            (+ index 1)
                                            dictionary))))
                 ((equal? 'endif command)
-                 (interpret-internal program
-                                     stack
+                 (interpret-internal stack
                                      (+ index 1)
                                      dictionary)))))))
