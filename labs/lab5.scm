85883cf55f53c6859b6c123b9a1256176836b8e8
Starovoytov Alexandr <saniy200311@gmail.com>
2021-12-01 12:53:30 +0300

    move out unit-tests to separate file

--- labs/lab5.scm
+++ labs/lab5.scm
@@ -113,7 +113,7 @@
                 ((assoc command dictionary) => (lambda (called-proc)
                                                  (loop (+ index 1)
                                                        dictionary
-                                                       (interpret-sub(trace-ex (cadr called-proc)) stack dictionary))))
+                                                       (interpret-sub (cadr called-proc) stack dictionary))))
                 ((equal? 'exit command) stack)
                 ((equal? 'if command)
                  (let* ((body (iffish))
@@ -133,110 +133,3 @@
 (define (interpret program stack)
   (interpret-sub program stack '()))
 
-(load "../unit-test.scm")
-
-(define tests
-  (list (test (interpret #(1) '()) (1))
-        (test (interpret #(1 2) '(10)) (2 1 10))
-        (test (interpret #(1 2 +) '(10)) (3 10))
-        (test (interpret #(1 2 -) '(10)) (-1 10))
-        (test (interpret #(1 2 *) '(10)) (2 10))
-        (test (interpret #(1 2 /) '(10)) (0 10))
-        (test (interpret #(1 2 mod) '(10)) (1 10))
-        (test (interpret #(1 2 neg) '(10)) (-2 1 10))
-        (test (interpret #(2 3 * 4 5 * +) '()) (26))
-        (test (interpret #(10 10 =) '()) (-1))
-        (test (interpret #(10 0 >) '()) (-1))
-        (test (interpret #(0 10 <) '()) (-1))
-        (test (interpret #(10 5 =) '()) (0))
-        (test (interpret #(0 10 >) '()) (0))
-        (test (interpret #(10 0 <) '()) (0))
-        (test (interpret #(0 0 and) '()) (0))
-        (test (interpret #(1000 7 -) '()) (993))
-        (test (interpret #(100 100 and) '()) (-1))
-        (test (interpret #(100 0 or) '()) (-1))
-        (test (interpret #(100 not) '()) (0))
-        (test (interpret #(0 not) '()) (-1))
-        (test (interpret #(   define -- 1 - end
-                            5 -- --      )  '()) (3))
-        (test (interpret #(10 15 +
-                              define -- 1 - end
-                              exit
-                              5 -- --) '()) (25))
-        (test (interpret #(10 15 +
-                              define -- exit 1 - end
-                              5 -- --) '()) (5 25)) ;; TODO списать лабу
-        (test (interpret #(10 4 dup) '()) (4 4 10))
-        (test (interpret #(   define abs
-                            dup 0 <
-                            if neg endif
-                            end
-                            9 abs
-                            -9 abs
-                            10 abs
-                            -10 abs) (quote ()))
-              (10 10 9 9))
-
-        (test (interpret #(   define =0? dup 0 = end
-                            define <0? dup 0 < end
-                            define signum
-                            =0? if exit endif
-                            <0? if drop -1 exit endif
-                            drop
-                            1
-                            end
-                            0 signum
-                            -5 signum
-                            10 signum       ) (quote ()))
-              (1 -1 0))
-
-        (test (interpret #(   define -- 1 - end
-                            define =0? dup 0 = end
-                            define =1? dup 1 = end
-                            define factorial
-                            =0? if drop 1 exit endif
-                            =1? if drop 1 exit endif
-                            dup --
-                            factorial
-                            *
-                            end
-                            0 factorial
-                            1 factorial
-                            2 factorial
-                            3 factorial
-                            4 factorial     ) (quote ()))
-              (24 6 2 1 1))
-
-        (test (interpret #(   define =0? dup 0 = end
-                            define =1? dup 1 = end
-                            define -- 1 - end
-                            define fib
-                            =0? if drop 0 exit endif
-                            =1? if drop 1 exit endif
-                            -- dup
-                            -- fib
-                            swap fib
-                            +
-                            end
-                            define make-fib
-                            dup 0 < if drop exit endif
-                            dup fib
-                            swap --
-                            make-fib
-                            end
-                            10 make-fib     ) (quote ()))
-              (0 1 1 2 3 5 8 13 21 34 55))
-
-        (test (interpret #(   define =0? dup 0 = end
-                            define gcd
-                            =0? if drop exit endif
-                            swap over mod
-                            gcd
-                            end
-                            90 99 gcd
-                            234 8100 gcd    ) '())
-              (18 9))
-        (test (interpret #(define =0? dup 0 = end =0?) '(0)) (-1 0))
-        (test (interpret #(define =0? dup 0 = end define kek 0 =0? end kek) '()) (-1 0))))
-
-(run-tests tests)
