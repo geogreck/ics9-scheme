af4205834e7a4abad31cab9c7e9ac4ff6eb376ad
Starovoytov Alexandr <saniy200311@gmail.com>
2021-12-02 17:52:56 +0300

    nested =if=, add =else=

---
+++ hw/hw5-tests.scm
@@ -0,0 +1,169 @@
+
+(load "./hw5.scm")
+(load "../unit-test.scm")
+
+(define tests
+  (list (test (interpret #(1) '()) (1))
+        (test (interpret #(1 2) '(10)) (2 1 10))
+        (test (interpret #(1 2 +) '(10)) (3 10))
+        (test (interpret #(1 2 -) '(10)) (-1 10))
+        (test (interpret #(1 2 *) '(10)) (2 10))
+        (test (interpret #(1 2 /) '(10)) (0 10))
+        (test (interpret #(1 2 mod) '(10)) (1 10))
+        (test (interpret #(1 2 neg) '(10)) (-2 1 10))
+        (test (interpret #(2 3 * 4 5 * +) '()) (26))
+        (test (interpret #(10 10 =) '()) (-1))
+        (test (interpret #(10 0 >) '()) (-1))
+        (test (interpret #(0 10 <) '()) (-1))
+        (test (interpret #(10 5 =) '()) (0))
+        (test (interpret #(0 10 >) '()) (0))
+        (test (interpret #(10 0 <) '()) (0))
+        (test (interpret #(0 0 and) '()) (0))
+        (test (interpret #(1000 7 -) '()) (993))
+        (test (interpret #(100 100 and) '()) (-1))
+        (test (interpret #(100 0 or) '()) (-1))
+        (test (interpret #(100 not) '()) (0))
+        (test (interpret #(0 not) '()) (-1))
+        (test (interpret #(define -- 1 - end
+                           5 -- --)
+                         '())
+              (3))
+        (test (interpret #(10 15 +
+                           define -- 1 - end
+                           exit
+                           5 -- --)
+                         '())
+              (25))
+        (test (interpret #(10 15 +
+                           define -- exit 1 - end
+                           5 -- --)
+                         '())
+              (5 25))
+        (test (interpret #(10 4 dup) '()) (4 4 10))
+        (test (interpret #(define abs
+                           dup 0 <
+                           if neg endif
+                           end
+                           9 abs
+                           -9 abs
+                           10 abs
+                           -10 abs)
+                         '())
+              (10 10 9 9))
+
+        (test (interpret #(define =0? dup 0 = end
+                           define <0? dup 0 < end
+                           define signum
+                           =0? if exit endif
+                           <0? if drop -1 exit endif
+                           drop
+                           1
+                           end
+                           0 signum
+                           -5 signum
+                           10 signum)
+                         '())
+              (1 -1 0))
+
+        (test (interpret #(define -- 1 - end
+                           define =0? dup 0 = end
+                           define =1? dup 1 = end
+                           define factorial
+                           =0? if drop 1 exit endif
+                           =1? if drop 1 exit endif
+                           dup --
+                           factorial
+                           *
+                           end
+                           0 factorial
+                           1 factorial
+                           2 factorial
+                           3 factorial
+                           4 factorial)
+                         '())
+              (24 6 2 1 1))
+
+        (test (interpret #(define =0? dup 0 = end
+                           define =1? dup 1 = end
+                           define -- 1 - end
+                           define fib
+                           =0? if drop 0 exit endif
+                           =1? if drop 1 exit endif
+                           -- dup
+                           -- fib
+                           swap fib
+                           +
+                           end
+                           define make-fib
+                           dup 0 < if drop exit endif
+                           dup fib
+                           swap --
+                           make-fib
+                           end
+                           10 make-fib)
+                         '())
+              (0 1 1 2 3 5 8 13 21 34 55))
+
+        (test (interpret #(define =0? dup 0 = end
+                           define gcd
+                           =0? if drop exit endif
+                           swap over mod
+                           gcd
+                           end
+                           90 99 gcd
+                           234 8100 gcd)
+                         '())
+              (18 9))
+        (test (interpret #(define =0? dup 0 = end =0?) '(0)) (-1 0))
+        (test (interpret #(define =0? dup 0 = end
+                           define kek 0 =0? end
+                           kek)
+                         '())
+              (-1 0))
+        (test (interpret #(define =0? dup 0 = end
+                          dup
+                          =0?
+                          if
+                              dup
+                              =0?
+                              if
+                                115
+                              endif
+                              20
+                          endif) '(0))
+              (20 115 0 0 0))
+        (test (interpret #(define =0? dup 0 = end
+                          dup
+                          =0?
+                          if
+                              dup
+                              =0?
+                              0
+                              if
+                                115
+                              else
+                                230
+                              endif
+                              20
+                          endif) '(0))
+              (20 230 -1 0 0 0))
+        (test (interpret #(define =0? dup 0 = end
+                            10
+                            =0? if
+                                115
+                                0
+                                    if =0?
+                                        100
+                                    endif
+                            else
+                                300
+                                1
+                                =0? if
+                                    4000
+                                else
+                                    700
+                                endif
+                            endif) '())
+              (700 1 300 10))))
+
+(run-tests tests)
---
+++ hw/hw5.scm
@@ -0,0 +1,139 @@
+;соавтор grechkogv@bmstu.ru
+;соавтор starovoytovai@bmstu.ru
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; соавтор аня шипель ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; - [X] реализуйте if с альтернативной веткой,
+;; - [X] реализуйте вложенные if,
+;; - [ ] циклы (с предусловием, с постусловием, с параметром),
+;; - [ ] выход из цикла и переход к новой итерации (break/continue),
+;; - [ ] конструкцией switch-case,
+;; - [ ] безымянными функциями и косвенным вызовом функций,
+;; - [ ] хвостовую рекурсию,
+;; - [ ] глобальные переменные в пространстве имен, отдельном от функций и пр.
+
+;; -> stack после выполнения program
+(define (interpret program stack)
+  (let interpret-internal ((stack stack)
+                           (index 0)
+                           (dictionary '()))
+    (if (= index (vector-length program))
+        stack
+        (let ((command (vector-ref program index)))
+          ;; выполнить операции со стеком, перейти к выполнению следующего слова
+          (let-syntax ((nxt-w-new-st (syntax-rules ()
+                                       ((_ new-stack)
+                                        (interpret-internal new-stack
+                                                            (+ index 1)
+                                                            dictionary)))))
+            (if (number? command)
+                (nxt-w-new-st (cons command stack))
+                (case command
+                  (+ (nxt-w-new-st (cons (+ (cadr stack) (car stack))
+                                         (cddr stack))))
+                  (- (nxt-w-new-st (cons (- (cadr stack) (car stack))
+                                         (cddr stack))))
+                  (* (nxt-w-new-st (cons (* (cadr stack) (car stack))
+                                         (cddr stack))))
+                  (/ (nxt-w-new-st (cons (quotient (cadr stack) (car stack))
+                                         (cddr stack))))
+                  (mod (nxt-w-new-st (cons (remainder (cadr stack) (car stack))
+                                           (cddr stack))))
+                  (neg (nxt-w-new-st (cons (- (car stack))
+                                           (cdr stack))))
+                  (= (nxt-w-new-st (cons (if (= (cadr stack) (car stack))
+                                             -1
+                                             0)
+                                         (cddr stack))))
+                  (< (nxt-w-new-st (cons (if (< (cadr stack) (car stack))
+                                             -1
+                                             0)
+                                         (cddr stack))))
+                  (> (nxt-w-new-st (cons (if (> (cadr stack) (car stack))
+                                             -1
+                                             0)
+                                         (cddr stack))))
+                  (not (nxt-w-new-st (cons (if (equal? (car stack) 0)
+                                               -1
+                                               0)
+                                           (cdr stack))))
+                  (and (nxt-w-new-st (cons (if (and (not (equal? (car stack) 0))
+                                                    (not (equal? (cadr stack) 0)))
+                                               -1
+                                               0)
+                                           (cddr stack))))
+                  (or (nxt-w-new-st (cons (if (or (not (equal? (car stack) 0))
+                                                  (not (equal? (cadr stack) 0)))
+                                              -1
+                                              0)
+                                          (cddr stack))))
+                  (drop (nxt-w-new-st (cdr stack)))
+                  (swap (nxt-w-new-st (cons (cadr stack)
+                                            (cons (car stack) (cddr stack)))))
+                  (dup (nxt-w-new-st (cons (car stack) stack)))
+                  (over (nxt-w-new-st (cons (cadr stack) stack)))
+                  (rot (nxt-w-new-st (cons (caddr stack)
+                                           (cons (cadr stack)
+                                                 (cons (car stack) (cdddr stack))))))
+                  (depth (nxt-w-new-st (cons (length stack) stack)))
+                  (define (interpret-internal stack
+                                              (+ 1 ;; следующее слово за end
+                                                 (let loop ((index (+ index 2))) ;; вернет индекс следующего end
+                                                   (if (eqv? 'end (vector-ref program index))
+                                                       index
+                                                       (loop (+ index 1)))))
+                                              (cons (list
+                                                     (vector-ref program (+ index 1))
+                                                     (+ index 2)) ;; (название-статьи индекс-первого-слова-статьи)
+                                                    dictionary)))
+                  (end stack)
+                  (exit stack)
+                  (if (if (= 0 (car stack))
+                          ;; выполняем вторую ветку, если она есть
+                          (interpret-internal (cdr stack)
+                                              ;; скипаем слова до
+                                              ;; соответствующего по вложенности endif или elseif
+                                              ;; +1 (продолжаем выполнение со следующего слова)
+                                              ;; это что, ПСП?
+                                              (+ 1
+                                                 (let loop ((index (+ index 1))
+                                                            (balance 0))
+                                                   (let ((command (vector-ref program index)))
+                                                     (case command
+                                                       (if (loop (+ index 1) (+ balance 1)))
+
+                                                       (endif (if (= balance 0)
+                                                                  index
+                                                                  (loop (+ index 1) (- balance 1))))
+                                                       ;; в case нельзя использовать else,
+                                                       ;; поэтому обработаю его тут
+                                                       (else (if (eqv? 'else command)
+                                                                 (if (= balance 0)
+                                                                     index
+                                                                     (loop (+ index 1) balance))
+                                                                 (loop (+ index 1) balance)))))))
+                                              dictionary)
+                          ;; выполняем первую ветку
+                          ;; если встретится elseif - скипнется вторая ветка
+                          ;; если endif - выполняем следующее слово
+                          (nxt-w-new-st (cdr stack))))
+                  (endif (nxt-w-new-st stack))
+                  ;; в case нельзя использовать else, поэтому обработаю его тут
+                  (else (if (eqv? command 'else)
+                            (interpret-internal stack
+                                                (+ 1
+                                                   (let loop ((index (+ index 1))
+                                                              (balance 0))
+                                                     (case (vector-ref program index)
+                                                       (if (loop (+ index 1) (+ balance 1)))
+                                                       (endif (if (= balance 0)
+                                                                  index
+                                                                  (loop (+ index 1) (- balance 1))))
+                                                       (else (loop (+ index 1) balance)))))
+                                                dictionary)
+                            ;; ищем статью в словаре
+                            (let ((jmp-index (cadr (assoc command dictionary))))
+                              (nxt-w-new-st (interpret-internal stack
+                                                                jmp-index
+                                                                dictionary))))))))))))
